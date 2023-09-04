568f753f63f5e1e8b92fcd8e3bf21ef54b22e7c2
oluobamzy <oluobamzy@gmail.com>
2023-09-04 10:51:03 -0400

    files to delete

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,13 @@
 import "./App.css";
 import "./components/Item.css";
 import Home from "./components/Home";
-import ListItem from "./components/ListItem";
-import ItemDetails from "./components/ItemDetails";
+
 
 function App() {
-  const imageUrlList = [
-    { id: 1, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-    { id: 2, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-    { id: 3, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-    { id: 4, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-    { id: 5, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-    { id: 6, url: "https://th.bing.com/th/id/OIP.t3YAUGbDG8BMGJo7Wq84MAHaJo?pid=ImgDet&rs=1" },
-  ];
 
-  // const imageList = imageUrlList.map((image)=>{
-  //   return <ListItem key = {image.id} imageUrl={image.url} />
-  // })
   return (
     <div className="App">
-      {/* <Home /> */}
-      {/* <div className="list-container">
-      <ListItem imageList = {imageUrlList}/>
-      </div> */}
-      <ItemDetails />
-      
+      <Home />
     </div>
   );
 }

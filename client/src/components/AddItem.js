2399401764fafc39c4ae560914f99ee11a47d7f3
oluobamzy <oluobamzy@gmail.com>
2023-09-07 18:36:29 -0400

    fixing-addiem

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -6,21 +6,25 @@ import { styled } from '@mui/system';
 import { Paper } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddItem() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    file: null,
-    itemName: "",
+    img_src: null,
+    item_name: "",
+    category: "",
+    color: "",
+    purchase_date: "",
+    closet_id: "",
     description: "",
     season: "",
-    category: "",
-    brandName: "",
-    colour: "",
+    last_worn_date: "",
     size: "",
-    lastWornDate: "",
-    purchaseDate: ""
+    brand_name: "",
   });
 
+  console.log("FORMDATE ------------->", formData)
   // State to hold the URL of the selected image for preview
   const [imagePreview, setImagePreview] = useState(null);
 
@@ -29,23 +33,46 @@ export default function AddItem() {
     flexDirection: 'column',
     alignItems: 'center',
     minHeight: '100vh',
-    backgroundColor: "#F1F0E8",
+    //backgroundColor: "#F1F0E8",
   }));
 
   const submit = async (event) => {
     event.preventDefault();
-
+  
     // Access the form data from the state object
-    const { file, itemName, description, brand, season, category, colour, size, lastWornDate, purchaseDate } = formData;
-
+    const { img_src, item_name, category, color, purchase_date, closet_id, description, season, last_worn_date, size, brand_name } = formData;
+  
     // Send the form data to the server
+    await fetch("/api/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Response------->", response)
+        return response.json();
+      })
+      .then((data) => {
+        console.log("User data added successfully:", data);
+        navigate('/items');
+        // Handle success
+      })
+      .catch((error) => {
+        console.error("Error adding user data:", error);
+        // Handle error
+      });
   };
-
   const handleInputChange = (event) => {
     const { name, value, files } = event.target;
 
     // If the input is a file input, update the 'file' property
-    if (name === "file") {
+    if (name === "img_src") {
+      console.log("FormImage---------->", name)
       setFormData({
         ...formData,
         [name]: files[0], // Use the selected file
@@ -70,75 +97,79 @@ export default function AddItem() {
   return (
     <div className="AddItem">
       <ResponsiveAppBar />
-      <StyledContainer>
-        <form onSubmit={submit} style={{ display: "flex", flexDirection: "column", marginTop: "200px" }}>
+        <form onSubmit={submit} style={{ display: "flex", flexDirection: "column", marginTop: "200px", maxWidth:"300px", justifyContent:"center",alignItems:"center" }}>
           {/* Image Preview */}
           {imagePreview && <img src={imagePreview} alt="Preview" style={{ maxWidth: '150px', maxHeight: '150px', marginBottom:"20px" }} />}
           <input
-            name="file"
-            onChange={handleInputChange}
+            name="img_src"
             type="file"
             accept="image/*"
+            onChange={handleInputChange}
           />
           <input
             name="description"
-            onChange={handleInputChange}
             type="text"
             placeholder="Description"
+            onChange={handleInputChange}
           />
           <input
-            name="itemName"
-            onChange={handleInputChange}
+            name="item_name"
             type="text"
             placeholder="Name"
+            onChange={handleInputChange}
           />
           <input
-            name="brandName"
-            onChange={handleInputChange}
+            name="brand_name"
             type="text"
             placeholder="Brand Name"
+            onChange={handleInputChange}
           />
           <input
             name="season"
             type="text"
-            onChange={handleInputChange}
             placeholder="season"
-          />
+            onChange={handleInputChange}
+            />
           <input
             name="category"
             type="text"
-            onChange={handleInputChange}
             placeholder="category"
-          />
-          <input
-            name="colour"
-            type="text"
             onChange={handleInputChange}
-            placeholder="colour"
-          />
+            />
+          <input
+            name="color"
+            type="text"
+            placeholder="color"
+            onChange={handleInputChange}
+            />
           <input
             name="size"
             type="text"
-            onChange={handleInputChange}
             placeholder="size"
-          />
-          <input
-            name="lastWornDate"
-            type="date"
             onChange={handleInputChange}
+            />
+          <input
+            name="last_worn_date"
+            type="date"
             placeholder="lastWornDate"
-          />
-          <input
-            name="purchaseDate"
-            type="date"
             onChange={handleInputChange}
+            />
+          <input
+            name="purchase_date"
+            type="date"
             placeholder="purchaseDate"
+            onChange={handleInputChange}
+            />
+            <input
+            name="closet_id"
+            type="text"
+            placeholder="closet_name"
+            onChange={handleInputChange}
           />
           <div className='addItem-btn'>
             <Button type="submit" style={{ backgroundColor: "#96B6C5" }}>Submit</Button>
           </div>
         </form>
-      </StyledContainer>
       <Footer />
     </div>
   );
--- client/src/components/Bin.js
+++ client/src/components/Bin.js
@@ -1,8 +1,34 @@
 import Item from "./Item";
 import CabinIcon from '@mui/icons-material/Cabin';
 import "./Bin.css";
+import {styled} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import Footer from "./Footer";
 
 const Bin = () => {
+  const StyledContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh', // Use minHeight to fill the entire viewport vertically
+    backgroundColor: "#F1F0E8",
+
+  }));
+
+  const StyledBanner = styled(Paper)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: theme.spacing(2),
+    backgroundColor: "#F1F0E8", // Background color
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    height: "50%",
+  }
+  ));
   const imageUrlList = [
     {
       id: 1,
@@ -23,6 +49,8 @@ const Bin = () => {
   });
   return (
     <div className="bin list">
+      <ResponsiveAppBar position="static" />
+      <StyledContainer>
       <div className="header">
         <CabinIcon className="cabinIcon"/>
         <h1>Bin</h1>
@@ -30,7 +58,8 @@ const Bin = () => {
       <div className="list-item">
       {image}
       </div>
-      
+      </StyledContainer>
+      <Footer />
     </div>
   )
 }

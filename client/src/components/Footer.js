92dc0b16746009aa08338f0f4334ad3e188bd923
Benson <bensonwlee@gmail.com>
2023-09-12 00:47:50 -0400

    fix merge

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,11 +7,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 
 const FooterContainer = styled(Grid)(({ theme }) => ({
   backgroundColor: "#ADC4CE",
-<<<<<<< HEAD
-  padding: theme.spacing(2),
-=======
   padding: theme.spacing(2), // Adjust the padding here for the footer container
->>>>>>> 5cd85a2a3c7f397d479af41aa9795922b8301492
   bottom: 0,
   left: 0,
   right: 0,
@@ -27,15 +23,9 @@ const Section = styled(Grid)(({ theme }) => ({
 }));
 
 const sectionTypographyStyles = {
-<<<<<<< HEAD
   fontFamily: "YourFontName, sans-serif", // Replace 'YourFontName' with the desired font name
   fontWeight: "bold", // You can adjust the font weight
   fontSize: "1.2rem", // You can adjust the font size
-=======
-  fontFamily: 'YourFontName, sans-serif', // Replace 'YourFontName' with the desired font name
-  fontWeight: 'bold', // You can adjust the font weight
-  fontSize: '1.2rem', // You can adjust the font size
->>>>>>> 5cd85a2a3c7f397d479af41aa9795922b8301492
 };
 
 const SocialLinks = styled("div")({
@@ -53,7 +43,6 @@ const Footer = () => {
   return (
     <FooterContainer container>
       <Section item xs={12} md={3}>
-<<<<<<< HEAD
         <Typography
           variant="h6"
           className="footer-section-title"
@@ -100,46 +89,6 @@ const Footer = () => {
           className="footer-section-title"
           style={sectionTypographyStyles}
         >
-=======
-        <Typography variant="h6" className="footer-section-title" style={sectionTypographyStyles}>
-          About Us
-        </Typography>
-        <Typography variant="body2">
-          Welcome to our wardrobe helper, Closet-YZ! We understand that choosing the perfect outfit
-          can be a daily challenge. Our mission is to give you a overview of your closet, and simplify your wardrobe manager. 
-        </Typography>
-      </Section>
-      <Section item xs={12} md={3}>
-        <Typography variant="h6" className="footer-section-title" style={sectionTypographyStyles}>
-          Meet the Team
-        </Typography>
-        <Typography variant="body2">
-          - Olu 
-        </Typography>
-        <Typography variant="body2">
-          - Ben
-        </Typography>
-        <Typography variant="body2">
-          - Khalid
-        </Typography>
-      </Section>
-      <Section item xs={12} md={3}>
-        <Typography variant="h6" className="footer-section-title" style={sectionTypographyStyles}>
-          Contact Us
-        </Typography>
-        <Typography variant="body2">
-          Email: <Link href="mailto:closet-yz@gmail.com">closet-yz@gmail.com</Link>
-        </Typography>
-        <Typography variant="body2">
-          City: Toronto, Ontario, Canada
-        </Typography>
-        <Typography variant="body2">
-          Office: Your Office Address
-        </Typography>
-      </Section>
-      <Section item xs={12} md={3}>
-        <Typography variant="h6" className="footer-section-title" style={sectionTypographyStyles}>
->>>>>>> 5cd85a2a3c7f397d479af41aa9795922b8301492
           Socials
         </Typography>
         <SocialLinks>
@@ -155,7 +104,6 @@ const Footer = () => {
         </SocialLinks>
       </Section>
       <Section item xs={12}>
-<<<<<<< HEAD
         <Typography
           variant="body2"
           align="center"
@@ -164,10 +112,6 @@ const Footer = () => {
           <Link href="/privacy-policy">Privacy Policy</Link> |{" "}
           <Link href="/terms-of-service">Terms of Service</Link> | © 2023
           Closet-YZ. All Rights Reserved.
-=======
-        <Typography variant="body2" align="center" style={{ marginTop: "16px" }}>
-          <Link href="/privacy-policy">Privacy Policy</Link> | <Link href="/terms-of-service">Terms of Service</Link> | © 2023 Closet-YZ. All Rights Reserved.
->>>>>>> 5cd85a2a3c7f397d479af41aa9795922b8301492
         </Typography>
       </Section>
     </FooterContainer>
